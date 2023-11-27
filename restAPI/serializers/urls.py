01230dc5912ec9a9dee7f6a5e2e052b45f85295e
Prabhat Tuladhar <PRABHATTULADHAR@GMAIL.COM>
2023-11-27 15:44:15 -0500

    removed

--- restAPI/serializers/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import PersonListView
-
-urlpatterns = [
-    path('persons/', PersonListView.as_view(), name='person-list'),
-]
--- restAPI/serializers/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import generics
-from .models import Person
-from .serializers import PersonSerializer
-
-class PersonListView(generics.ListCreateAPIView):
-    queryset = Person.objects.all()
-    serializer_class = PersonSerializer
